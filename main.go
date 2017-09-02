224d82bc0812c9a5306b00b4915664ca0a38a640
Divan Visagie <visagiedivan@gmail.com>
2017-09-02 10:17:51 +0200

    now reads from json config

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
+type Proxy struct {
+	url    string
+	target string
+}
+
+func readFile() []Proxy {
+	file, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	jsonMap := make(map[string]string)
+
+	unmarshalError := json.Unmarshal(file, &jsonMap)
+	if unmarshalError != nil {
+		log.Fatalln(unmarshalError.Error())
+	}
+
+	var proxies = make([]Proxy, len(jsonMap))
+
+	i := 0
+	for k, v := range jsonMap {
+		proxies[i] = Proxy{string(k), string(v)}
+		i++
+	}
+
+	return proxies
+}
+
 func main() {
-	fmt.Println("Hello World")
+	proxies := readFile()
+
+	for _, proxy := range proxies {
+		fmt.Printf("url: %s target: %s \n", proxy.url, proxy.target)
+	}
 }
