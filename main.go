ef7857eba0cc2e6ae4429ca859fa190de7162cda
Divan Visagie <me@divanv.com>
2024-06-08 01:30:23 +0200

    Improved argument parsing
    Changed from raw parser to self implemented struct based parser

--- main.go
+++ main.go
@@ -16,30 +16,34 @@ func setupReverseProxy(path string, port string, target string) {
 	http.ListenAndServe(port, nil)
 }
 
+// declare args struct to contain args
+type args struct {
+	port   string
+	target string
+	path   string
+}
+
+// create associated function on args struct
+func (a *args) parseArgs() {
+	//parse port
+	if len(os.Args) < 2 {
+		log.Fatalln("You need to pass in port and target parameters: reverse-proxy :8080 http:\\\\example.com")
+	}
+
+	a.port = os.Args[1]
+	a.target = os.Args[2]
+	a.path = "/"
+}
+
 func main() {
 	path := "/"
 	port := ":8080"
 	target := "http://www.google.com"
 
-	args := os.Args[1:]
-	if len(args) < 1 {
-		errorMessage := "You need to pass in port and target parameters: reverse-proxy :8080 http:\\\\example.com"
-		log.Fatalln(errorMessage)
-	}
+	args := args{}
+	args.parseArgs()
 
-	if len(args) == 1 {
-		errorMessage := "You need to provide a target for your proxy"
-		log.Fatalln(errorMessage)
-	}
-
-	if len(args) == 3 {
-		path = args[2]
-	}
-
-	port = args[0]
-	target = args[1]
-
-	fmt.Printf("Proxying calls to %s%s to %s", port, path, target)
+	fmt.Printf("Proxying calls to %s%s to %s", args.port, args.path, args.target)
 
 	setupReverseProxy(path, port, target)
 }
